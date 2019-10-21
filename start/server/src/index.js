15c6166689e2ce9e45a71124ca6e5cb124646fed
AlphaDu <alphadu811@outlook.com>
2019-10-21 19:49:16 +0800

    s

---
+++ start/server/src/index.js
@@ -0,0 +1,7 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+
+const server = new ApolloServer({ typeDefs });
+server.listen(4000).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
