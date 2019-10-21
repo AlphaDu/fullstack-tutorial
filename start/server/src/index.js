fdb750331dacd7d25cbc21f718da0d1a63f164cc
AlphaDu <alphadu811@outlook.com>
2019-10-21 20:03:06 +0800

    save

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,7 +1,14 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 server.listen(4000).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
